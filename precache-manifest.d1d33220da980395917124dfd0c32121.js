15f405d62194f72712467ac91e6d0c442492b497
Himanshu Jain <hjmanojain.8@gmail.com>
2020-05-06 22:22:01 +0530

    Updates

---
+++ precache-manifest.d1d33220da980395917124dfd0c32121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad472cf1ea01b1d774d9ea957c537e57",
+    "url": "/chess-ai/index.html"
+  },
+  {
+    "revision": "565ceb1221a0a6118246",
+    "url": "/chess-ai/static/css/main.6fa0b717.chunk.css"
+  },
+  {
+    "revision": "4400297e32bb19342a01",
+    "url": "/chess-ai/static/js/2.4367583c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess-ai/static/js/2.4367583c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565ceb1221a0a6118246",
+    "url": "/chess-ai/static/js/main.277f54da.chunk.js"
+  },
+  {
+    "revision": "527b0bd899787f17b551",
+    "url": "/chess-ai/static/js/runtime-main.fce34246.js"
+  }
+]);
